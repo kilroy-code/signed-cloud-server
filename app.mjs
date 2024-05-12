6992be632de80a697196565596dd90cb1fc7aa16
Howard (Aech Linden) Stearns <howard.stearns@gmail.com>
2024-05-12 11:22:53 -0700

    cors allowing ki1r0y.com

--- app.mjs
+++ app.mjs
@@ -3,6 +3,7 @@ import path from 'path';
 import * as fs from 'node:fs/promises';
 import express from 'express';
 import logger from 'morgan';
+import cors from 'cors';
 import {keys, origin} from './lib/storage.mjs';
 import pkg from './package.json' assert {type: 'json'};
 process.title = 'ki1r0ystore';
@@ -10,12 +11,18 @@ process.title = 'ki1r0ystore';
 const port = new URL(origin).port;
 const app = express();
 app.use(logger(':date[iso] :method :url :status :res[content-length] :response-time '));
+
+app.use(cors({origin: 'https://kilr0.com'}))
 app.use('/db', keys);
 
-for (let relative of ['node_modules/@kilroy-code/kilroy/public', '../..', '../../../..']) {
+// There are three places where ki1r0y stuff might be, depending on how things are installed.
+for (let relative of [
+  'node_modules/@kilroy-code/kilroy/public', // ki1r0y package installed as a dev dependency of signed-cloud-server, for GitHub actions.
+  '../..',                                   // signed-cloud-server repo cloned under a server app's public directory.
+  '../../../..'                              // signed-cloud-server package installed as a dependency of a server app.
+]) {
   let real = await fs.realpath(relative).catch(() => {});
   if (real) {
-    console.log({relative, real});
     app.use(express.static(real)); // { maxAge: '1h'}
   }
 }
